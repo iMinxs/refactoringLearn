3c7860fee64e65ef62552edb0ad7bbfba3fc45e1
iMinxs <iminxs@163.com>
2020-12-11 22:39:42 +0800

    Refinement function & Variable rename

--- statement.js
+++ statement.js
@@ -1,40 +1,11 @@
-const plays = {
-  hamlet: {
-    name: "Hamlet",
-    type: "tragedy",
-  },
-  "as-like": {
-    name: "As You Like It",
-    type: "comedy",
-  },
-  othello: {
-    name: "Othello",
-    type: "tragedy",
-  },
-};
-
-const invoices = {
-  customer: "BogCo",
-  performances: [
-    {
-      playID: "hamlet",
-      audience: 55,
-    },
-    {
-      playID: "as-like",
-      audience: 35,
-    },
-    {
-      playID: "othello",
-      audience: 40,
-    },
-  ],
-};
+const plays = require("./plays");
+const invoices = require("./invoices");
 
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
+  let result = `-------------------------------------\n`;
+  result += `Statement for ${invoice.customer}\n`;
   const format = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
@@ -43,25 +14,8 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
 
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play);
 
     //add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -78,8 +32,35 @@ function statement(invoice, plays) {
 
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
+  result += `-------------------------------------\n`;
 
   return result;
 }
 
-console.log(statement(invoices, plays));
+function amountFor(perf, play) {
+  let result = 0;
+
+  switch (play.type) {
+    case "tragedy":
+      result = 40000;
+      if (perf.audience > 30) {
+        result += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      result = 30000;
+      if (perf.audience > 20) {
+        result += 10000 + 500 * (perf.audience - 20);
+      }
+      result += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+
+  return result;
+}
+
+for (let i in invoices) {
+  console.log(statement(invoices[i], plays));
+}
